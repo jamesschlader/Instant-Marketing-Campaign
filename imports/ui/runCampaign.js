0bee18082eda117ca7090a8ada9b541172d98155
Jim Schlader <jamesschlader@yahoo.com>
2019-06-18 14:20:01 -0600

    currently generating as many as 1000 new customers, showing the new customers, and displaying all the customers in a table. Can delete customers. Next step to save customer list to file.

--- imports/ui/runCampaign.js
+++ imports/ui/runCampaign.js
@@ -1,2 +1,37 @@
 import "./runCampaign.html";
+import { customer } from "../api/createCustomer";
+import { Customers } from "../api/Customers";
 import { Template } from "meteor/templating";
+import { ReactiveDict } from "meteor/reactive-dict";
+
+Template.runCampaign.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  this.state.set("newbies", []);
+});
+
+Template.runCampaign.helpers({
+  newbiesCount() {
+    const instance = Template.instance();
+    return instance.state.get("newbies").length;
+  },
+  newbies() {
+    const instance = Template.instance();
+    return instance.state.get("newbies");
+  }
+});
+
+Template.runCampaign.events({
+  "submit form"(event, instance) {
+    event.preventDefault();
+    const { target } = event;
+    const { value } = target.campaign;
+    let newCustomers = [];
+    console.log(value);
+    for (let i = 1; i < value + 1; i++) {
+      const newCustomer = customer();
+      newCustomers.push(newCustomer);
+      instance.state.set("newbies", newCustomers);
+      Customers.insert(newCustomer);
+    }
+  }
+});
--- imports/ui/showCustomers.js
+++ imports/ui/showCustomers.js
@@ -4,17 +4,55 @@ import { Customers } from "../api/Customers";
 import { month } from "../api/month";
 import { ReactiveDict } from "meteor/reactive-dict";
 
+Template.showCustomers.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+  this.state.set("showing", []);
+});
+
 Template.showCustomers.helpers({
   customers() {
     const customers = Customers.find({});
 
     return customers;
   },
+  showing(id) {
+    const instance = Template.instance();
 
+    if (instance.state.get("showing").length > 0) {
+      const result = instance.state.get("showing").filter(item => {
+        return item == id;
+      });
+      return result.length > 0 ? true : false;
+    } else {
+      return false;
+    }
+  },
   formatDate(date) {
     const dateString = `${month(
       date.getMonth()
     )} ${date.getDate()}, ${date.getFullYear()}`;
     return dateString;
+  },
+  customerCount() {
+    return Customers.find({}).count();
+  }
+});
+
+Template.showCustomers.events({
+  "click .delete"(event) {
+    event.preventDefault();
+    Customers.remove(this._id);
+  },
+  "click .home"(event, instance) {
+    event.preventDefault();
+    instance.state.set("showing", [...instance.state.get("showing"), this._id]);
+  },
+  "click .uk-button-danger"(event, instance) {
+    event.preventDefault();
+    const showings = instance.state.get("showing");
+    const filtered = showings.filter(item => {
+      return item !== this._id;
+    });
+    instance.state.set("showing", filtered);
   }
 });
